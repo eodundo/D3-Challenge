56314110822dedff375d202d4c2692773d788635
Elizabeth Odundo <Liz@Elizabeths-MacBook-Air.local>
2021-07-13 18:36:42 -0700

    changes

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -7,10 +7,10 @@ var svgHeight = 660;
 
 // Define the chart's margins as an object
 var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
 };
 
 // Define dimensions of the chart area
@@ -52,80 +52,62 @@ var xLinearScale = d3.scaleLinear()
     .range([chartHeight, 0])
     .domain([2, d3.max(Data, data => data.healthcare)]);
 
-    //Create Axis Functions
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
+    // create axes
+      var bottomAxis = d3.axisBottom(xLinearScale);
+      var leftAxis = d3.axisLeft(yLinearScale);
 
-//Adding in bottom and left axis
-chartGroup.append("g")
-  .attr("transform", 'translate (0, ${chartHeight})')
-  .call(bottomAxis);
-chartGroup.append("g")
-  .call(leftAxis);
+      // append axes
+      chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
 
-//Data points
-var circlesGroup = chartGroup.selectAll("circle")
-.data(Data)
-.enter()
-.append("circle")
-.attr("cx", (d,i) => xScale(d.poverty))
-.attr("cy", d => yscale(d.healthcare))
-attr("r", "15")
-.attr("fill", "blue")
-.classed("stateCircle", true)
+      chartGroup.append("g")
+        .call(leftAxis);
 
-//State abbreviations
-chartGroup.selectAll("text")
-.data(Data)
-.enter()
-.append("text")
-.attr("x" (d,i) => xScale(d.poverty))
-.attr("y", d => (yScale(d.healthcare-0.28)))
-.classed("stateText", true)
-.text(d => d.abbr)
-.on("mouseover", function(d) {
-  toolTip.show(d);
-})
-.on("mouseover", function(d,i) {
-  toolTip.hide(d);
+         // append circles
+      var circlesGroup = chartGroup.selectAll("circle")
+        .data(Data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "10")
+        .attr("fill", "blue")
+        .attr("opacity", "0.3")
+        .attr("stroke", "black");
+
+     chartGroup.append("g").selectAll("text")
+        .data(Data)
+        .enter()
+        .append("text")
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .attr("text-anchor", "middle")
+        .attr("font_family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("alignment-baseline", "central")
+        .attr("fill", "white")
+        .attr("font-weight", "bold");
+
+//Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - chartMargin.left -5)
+      .attr("x", 0 - (chartHeight / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top -10})`)
+      .text("In Poverty (%)");
+
+
+
+
+
+
+
 });
-
-// x labels
-chartGroup.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y" 0 -margin.left)
-  .attr("x", 0 - height / 2)
-  .attr("dy", "lem")
-  .classed("aText", true)
-  .attr("data-axis-name", "healthcare")
-  .text("Lacks Healthcare(%)");
-
-  //y labels
-  chartGroup.append("text")
-  .attr("transform", "translate(" + width / 2 + " , " + (height + margin.top + 20) + ")")
-  .attr("data-axis-name", "poverty")
-  .classed("aText", true)
-  .text("In Poverty (%");
-
-  // ToolTip
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([-10, 30])
-    .html(functions(d) {
-      return ('${d.abbr}<br>Healthcare (%): ${d.healthcare}%<br>Poverty: ${d.poverty}');
-    })
-
-// Intergrate ToolTip into chart
-chartGroup.call(toolTip)
-
-//Event Listener for display and hide of tooltip
-circlesGroup.on("mouseover", functions(d) {
-  toolTip.show(d);
-
-})
-    on("mouseout", function(d, i){
-      toolTip.hide(d);
-    })
-
-  });
 
